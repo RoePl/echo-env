894a7a0b188896da4f2332597040a371211f0e95
Roe Ploutno <roe.ploutno@gmail.com>
2024-08-05 13:04:02 +0300

    language classifier fixed

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
-    'Programming Language :: Python :: 3.6+',
+    'Programming Language :: Python :: 3.6',
   ],
 )
